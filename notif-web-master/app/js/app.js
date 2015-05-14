e5206e419933d09eb97e4903cd62778fe28c949b
Prashant <prashantsinha_492@yahoo.in>
2015-05-15 00:05:42 +0530

    notif-web changes

--- notif-web-master/app/js/app.js
+++ notif-web-master/app/js/app.js
@@ -5,31 +5,49 @@
 */
 var notifWeb = angular.module('notifWeb', []);
 
-notifWeb.factory('SendMessageService', ['$http', function($http){
-	var sendMessageService = {};
-
-	sendMessageService.checkNames = function() {		
-		return $http.get("email.json");
-	};
-
-	sendMessageService.sendMessage = function(message) {
-		$http.put("message.json", message);
-	}
-
-	return sendMessageService;
-}]);
-
 notifWeb.controller('sendMessageCtrl', ['$scope', 'SendMessageService',function($scope, SendMessageService){
-	$scope.message={};
-	$scope.message.contenttype="text";
-	$scope.message.text=true;
+	
+	/*
+	*	Default Values
+	*/
 	$scope.contenttypes=[
 		{label:"File", value:"file"},
 		{label:"Text", value:"text"}];
+	$scope.errorMessage="Error sending message!";
+	$scope.successMessage="Message sent successfully!";
+
+	/*
+	* Message Structure Defaults
+	*/
+	$scope.newMessage = function() {
+		$scope.disabled = false;
+		$scope.success = false;
+		$scope.error = false;
+		$scope.messagesent = false;
+		$scope.message={};
+		$scope.message.contenttype="text";
+		$scope.message.text=true;
+	}
+	$scope.newMessage(); // Load defaults initially
+
+	/*
+	*	Send Message
+	*/
 	$scope.sendMessage = function(){
 		console.log($scope.message);
-		SendMessageService.sendMessage($scope.message);
+		$scope.disabled = true;
+		$scope.messagesent = true;
+		SendMessageService.sendMessage($scope.message)
+			.success(function(repsonseData) {
+				$scope.disabled = true;
+				$scope.success = true;
+			})
+			.error(function(responseData) {
+				$scope.disabled = true;
+				$scope.error = true;
+			});
 	};
+
 	$scope.$watch('message.contenttype', function(newvalue, oldvalue){
 		if(newvalue) {
 			if(newvalue=="text") {
@@ -47,7 +65,7 @@ notifWeb.directive('emailTo', ['$http', 'SendMessageService', function($http, se
 	// Runs during compile
 	return {
 		restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
-		template: '<input id="to" type="text" class="form-control" placeholder="Enter Receipents" ng-model="message.to"></input>',		
+		template: '<input id="to" type="text" class="form-control" disableinput="disabled" placeholder="Enter Receipents" ng-model="message.to"></input>',		
 		link: function($scope, element, attr, controller) {
 			
 		    var split = function ( val ) {
@@ -100,4 +118,37 @@ notifWeb.directive('emailTo', ['$http', 'SendMessageService', function($http, se
 		      });
 		  }
 		}
+}]);
+
+notifWeb.directive('disableinput', [function(){
+	// Runs during compile
+	return {
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		link: function($scope, element, attr, controller) {
+			var varToWatch = attr['disableinput'];
+			$scope.$watch(varToWatch, function(newValue, oldValue) {
+				if(newValue) {
+					$(element).prop('disabled', true);
+					$(element).css("cursor","default");
+				} else {
+					$(element).prop('disabled', false);
+				}
+			});
+		}
+	}
+}]);
+
+
+notifWeb.factory('SendMessageService', ['$http', function($http){
+	var sendMessageService = {};
+
+	sendMessageService.checkNames = function() {		
+		return $http.get("email.json");
+	};
+
+	sendMessageService.sendMessage = function(message) {
+		return $http.put("message.json", message);
+	}
+
+	return sendMessageService;
 }]);
